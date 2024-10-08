cb1853a3afe04fac9f073ddc9d45780bff34c82d
Mariareissilva <147395613+Mariareissilva@users.noreply.github.com>
2024-10-08 08:49:37 -0300

    efe

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ function criaCartao(pergunta, resposta){
 let conteudo = document.getElementById ('conteudo')
  let cartao = document.createElement('article')
  cartao.className = 'cartao'
- console.log(cartao)
+ 
+cartao.innerHTML = `
+<div class= "cartao--conteudo">
+    <div class="cartao--pergunta"><p>Que cidade serve de cenário para o seriado?</p> </div>
+    <div class="cartao--resposta"><p>Miami</p></div>
+</div>
+`
+console.log(cartao)
 }
 
