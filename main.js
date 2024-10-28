a0be5fdd814b8767eee82ad7d63636700f07b5e0
Mariareissilva <147395613+Mariareissilva@users.noreply.github.com>
2024-10-28 10:00:28 -0300

    jdj

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ let conteudo = document.getElementById ('conteudo')
  
 cartao.innerHTML = `
 <div class= "cartao--conteudo">
-    <div class="cartao--pergunta"><p>Que cidade serve de cenário para o seriado?</p> </div>
-    <div class="cartao--resposta"><p>Miami</p></div>
+    <div class="cartao--pergunta"><p>$(pergunta)</p> </div>
+    <div class="cartao--resposta"><p>$(resposta)</p></div>
 </div>
 `
 console.log(cartao)
