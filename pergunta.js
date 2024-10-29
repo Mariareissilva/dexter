2b9611225e17aa29b3c21f2c0f290a4454098c2f
Mariareissilva <147395613+Mariareissilva@users.noreply.github.com>
2024-10-29 08:28:35 -0300

    gg

--- pergunta.js
+++ pergunta.js
@@ -1,3 +1,25 @@
 criaCartao(
-    "qual cidade serve de cenario para o seriado"
+    "qual cidade serve de cenario para o seriado",
+    "Miami"
 )
+criaCartao(
+    "Dexter trabalha como perito para a polícia de Miami. Qual é a sua especialidade?",
+    "Respingo de sangue"
+)
+criaCartao(
+"Para auxiliar nas investigações, o agente especial Frank Lundy aparece na 2 temporada e reaparece na 4. Qual músico Lundy gostava de escutar?",
+"Chopin"
+)
+    criaCartao(
+"Quem foi o assassino de Frank Lundy?",
+"Christine Hill"
+    )
+        criaCartao(
+"Qual a profissão que Arthur Mitchell usava como disfarce para esconder seu lado sombrio?",
+"Professor"
+        )
+            criaCartao(
+"Na quinta temporada Dexter ajuda uma mulher a se vingar dos homens que a machucaram no passado. Qual nome dessa mulher?",
+"Lumen"
+
+            )
